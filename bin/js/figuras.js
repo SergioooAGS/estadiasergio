9671ad4b909d92f8cbd97b0a06b0b8370b751666
SergioooAGS <serginchis@gmail.com>
2024-01-23 12:05:44 -0600

    Funcionalidad lista

--- bin/js/figuras.js
+++ bin/js/figuras.js
@@ -1,15 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const d3 = require("d3");
 var figuras;
 (function (figuras) {
-    function areaRectangulo() {
+    function calcularArea() {
         let areaResultado = d3.select("#areaResultadoRectangulo");
         let b = parseInt(document.getElementById("baseRectangulo").value);
         let h = parseInt(document.getElementById("alturaRectangulo").value);
         let area = b * h;
         areaResultado.html("" + area);
     }
-    figuras.areaRectangulo = areaRectangulo;
+    figuras.calcularArea = calcularArea;
 })(figuras || (figuras = {}));
 //# sourceMappingURL=figuras.js.map
--- src/figuras.ts
+++ src/figuras.ts
@@ -1,11 +1,10 @@
-import { area } from "d3";
-import d3 = require("d3");
 
 namespace figuras {
 
-        export function areaRectangulo() {
+        export function calcularArea() {
+               
                let areaResultado = d3.select("#areaResultadoRectangulo")
-               let b = parseInt((<HTMLInputElement>document.getElementById("baseRectangulo")).value);
+               let b = parseInt( (<HTMLInputElement>document.getElementById("baseRectangulo")).value);
                let h = parseInt((<HTMLInputElement>document.getElementById("alturaRectangulo")).value);
                let area = b * h;
                areaResultado.html("" + area);
