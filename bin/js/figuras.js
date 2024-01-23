29c5727cf6885f2e7fddba8114d25cb9395d2c6e
SergioooAGS <serginchis@gmail.com>
2024-01-23 15:56:28 -0600

    Cambios 2pte

--- bin/js/figuras.js
+++ bin/js/figuras.js
@@ -1,10 +1,17 @@
 var figuras;
 (function (figuras) {
+    function BtnCk() {
+        d3.select('#BtnCk').dispatch('click');
+    }
+    figuras.BtnCk = BtnCk;
     function calcularArea() {
-        let areaResultado = d3.select("#areaResultadoRectangulo");
-        let b = parseInt(document.getElementById("baseRectangulo").value);
-        let h = parseInt(document.getElementById("alturaRectangulo").value);
+        // let areaResultado = d3.select("#areaResultadoRectangulo")
+        let areaResultado = d3.select(("#areaResultadoRectangulo"))[""][""].value;
+        let b = parseFloat(d3.select("#baseRectangulo").property("value"));
+        let h = parseFloat(d3.select("#alturaRectangulo").property("value"));
         let area = b * h;
+        //let a = parseInt( (<HTMLInputElement>document.getElementById("baseRectangulo")).value);
+        //let h = parseInt((<HTMLInputElement>document.getElementById("alturaRectangulo")).value);
         areaResultado.html("" + area);
     }
     figuras.calcularArea = calcularArea;
--- src/figuras.ts
+++ src/figuras.ts
@@ -1,12 +1,23 @@
 
 namespace figuras {
 
+        export function BtnCk() {
+            d3.select('#BtnCk').dispatch('click');
+        }   
+
         export function calcularArea() {
+            
+// let areaResultado = d3.select("#areaResultadoRectangulo")
+               let areaResultado = d3.select(("#areaResultadoRectangulo"))[""][""].value;
+
+               let b = parseFloat(d3.select("#baseRectangulo").property("value"));            
+               let h = parseFloat(d3.select("#alturaRectangulo").property("value"));
                
-               let areaResultado = d3.select("#areaResultadoRectangulo")
-               let b = parseInt( (<HTMLInputElement>document.getElementById("baseRectangulo")).value);
-               let h = parseInt((<HTMLInputElement>document.getElementById("alturaRectangulo")).value);
                let area = b * h;
+
+               //let a = parseInt( (<HTMLInputElement>document.getElementById("baseRectangulo")).value);
+               //let h = parseInt((<HTMLInputElement>document.getElementById("alturaRectangulo")).value);
+               
                areaResultado.html("" + area);
            }
         }
